5541fbab7b41e12b549721362d7736d9878f93f3
Logan Murphy <lmurphy5@logan.attlocal.net>
2021-12-06 13:20:39 -0600

    add post views and components

---
+++ components/Post.js
@@ -0,0 +1,42 @@
+import Link from "next/link"
+import Image from "next/image"
+import CategoryLabel from "../components/CategoryLabel";
+
+export default function Post({ post }) {
+    return (
+        <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+            <Image src={post.frontMatter.cover_image} alt="" height={420} width={600} className="mb-4 rounded" />
+            <div className="flex justify-between items-center">
+                <span className="font-light gray-600">
+                    {post.frontMatter.date}
+                </span>
+                <div>
+                    <CategoryLabel>
+                        {post.frontMatter.category}
+                    </CategoryLabel>
+                </div>
+            </div>
+            <div className="mt-2">
+                <Link href={`/blog/${post.slug}`}>
+                    <a className="text-2xl text-gray-700 font-bold hover:underline">
+                        {post.frontMatter.title}
+                    </a>
+                </Link>
+                <p className="mt-2 text-gray-600">
+                    {post.frontMatter.excerpt}
+                </p>
+            </div>
+            <div className="flex justify-between item-center mt-6">
+                <Link href={`/blog/${post.slug}`}>
+                    <a className="text-gray-900  hover:text-blue-600">
+                        Read More
+                    </a>
+                </Link>
+                <div className="flex items-center">
+                    <img src={post.frontMatter.author_image} alt="author image" className="mx-4 w-10 object-cover rounded-full hidden sm:block" />
+                    <h3 className="text-gray-700 font-bold">{post.frontMatter.author}</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,40 @@
+import fs from "fs"
+import path from "path"
+import matter from 'gray-matter';
+import Link from "next/link"
 import Layout from "../../components/Layout"
+import Post from "../../components/Post"
+import { sortByDate } from "../../utils"
 
-export default function BlogPage() {
+export default function BlogPage({ posts }) {
     return (
         <Layout>
-            Blog
+            <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {posts.map((post, i) => (
+                    <Post key={i} post={post} />
+                ))}
+            </div>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join("posts"))
+    const posts = files.map(filename => {
+        const slug = filename.replace(".md", "")
+        const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8")
+        const { data: frontMatter } = matter(markdownWithMeta)
+
+        return {
+            slug,
+            frontMatter
+        }
+    })
+
+    return {
+        props: {
+            posts: posts.sort(sortByDate)
+        }
+    }
 }
